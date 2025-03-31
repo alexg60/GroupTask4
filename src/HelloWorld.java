cce4bc296a31aaf290b8d019a9dc410f95909fbd
alexgilbert <alexg6@umbc.edu>
2025-03-31 18:14:54 -0400

    changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("DockerHub, Github workspaces");
+        System.out.println("this is a another test message");
     }
 }
